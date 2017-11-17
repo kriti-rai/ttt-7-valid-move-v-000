4d013e4440de93f5be3599ee1225dc85ebe99def
Kriti Rai <kriti.rai@gmail.com>
2017-11-17 17:56:57 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -10,7 +10,7 @@ def valid_move?(board,index)
   if index.between?(0,8) && position_taken?(board, index) == false
     return true
   else
-    return false 
+    return false
   end
 end# code your #valid_move? method here
 
